869f1f9ec259ba788e8816eeb98a8a867fd9f940
GithubVanlinh <vovanlinhleo1999@gmail.com>
2020-11-14 18:02:47 +0700

    add link in model

--- models/product.model.js
+++ models/product.model.js
@@ -1,4 +1,4 @@
 module.exports = {
-    get: ()=> ([{id: "abcdefjk",name: "abc", price: 40000, discription: "some text", rate: 5},
+    get: ()=> ([{id: "abcdefjk",name: "abc", price: 40000, images:[{link: "1233"}, {link: "234"}], discription: "some text", rate: 5},
      {id: "abcdefjj", name:"cde", price: 20000, discription: "expensive", rate: 5}]),
 }
