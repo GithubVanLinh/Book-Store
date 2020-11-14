9384b68151e5421f17dc7b6985f8e649fa2df1e7
GithubVanlinh <vovanlinhleo1999@gmail.com>
2020-11-14 20:56:07 +0700

    add product model

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,11 +1,15 @@
-const productModel = require("../models/product.model");
 const ProductModel = require("../models/product.model");
 
 module.exports = {
   getProductList: (req, res, next) => {
-    console.log(productModel.get());
-    res.render("product/product-list", productModel.get());
+    // console.log(ProductModel.get());
+    res.render("product/product-list", { productList: ProductModel.get() });
   },
-  getProductById: (req, res, next) =>
-    res.render("product/product-detail", { title: "Express" }),
+  getProductById: (req, res, next) => {
+      const id = req.params.id;
+    //   console.log(id);
+      const product = ProductModel.getProductById(id);
+      console.log(product);
+    res.render("product/product-detail", { product});
+  },
 };
--- models/product.model.js
+++ models/product.model.js
@@ -1,4 +1,31 @@
+const products = [
+  {
+    id: "abcdefjk",
+    name: "abc",
+    price: 4000,
+    image:
+      "https://thumbs-prod.si-cdn.com/MXRigU6qbW9_2nLwBvXywDRQJ24=/1072x720/filters:no_upscale()/https://public-media.si-cdn.com/filer/91/91/91910c23-cae4-46f8-b7c9-e2b22b8c1710/lostbook.jpg",
+    catogery: ["story", "fantasy"],
+    description: "some text",
+  },
+  {
+    id: "abcdefjj",
+    name: "cde",
+    price: 2000,
+    image:
+      "https://www.incimages.com/uploaded_files/image/1920x1080/getty_690360222_20001333165376734769_408521.jpg",
+    catogery: ["story", "fantasy"],
+    description: "expensive",
+  },
+];
+
 module.exports = {
-    get: ()=> ([{id: "abcdefjk",name: "abc", price: 40000, images:[{link: "1233"}, {link: "234"}], discription: "some text", rate: 5},
-     {id: "abcdefjj", name:"cde", price: 20000, discription: "expensive", rate: 5}]),
-}
+  get: () => products,
+  getProductById: (id) => {
+    for (i of products) {
+      if (i.id === id) {
+        return i;
+      }
+    }
+  },
+};
